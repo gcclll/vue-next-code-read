9e97ea3f720e4dbd95b9d7d14822aba179850570
gcclll <gccll.love@gmail.com>
2020-05-25 20:45:58 +0800

    feat: map

--- packages/reactivity/collectionHandlers.js
+++ packages/reactivity/collectionHandlers.js
@@ -2,6 +2,7 @@ import { toRaw, reactive, readonly, ReactiveFlags } from './reactive.js'
 import { track, trigger, ITERATE_KEY, MAP_KEY_ITERATE_KEY } from './effect.js'
 import { hasChanged, hasOwn, isObject } from '../util.js'
 
+const __DEV__ = true
 const toReactive = (value) => (isObject(value) ? reactive(value) : value)
 const toReadonly = (value) => (isObject(value) ? readonly(value) : value)
 const toShallow = (value) => value
@@ -20,6 +21,77 @@ function get(target, key, wrap) {
   } else if (has.call(target, rawKey)) {
     return wrap(get.call(target, rawKey))
   }
+}
+
+function has(key) {
+  const target = toRaw(this)
+  const rawKey = toRaw(key)
+  if (key !== rawKey) {
+    track(target, 'has', key)
+  }
+  track(target, 'has', rawKey)
+
+  const has = getProto(target).has
+  return has.call(target, key) || has.call(target, rawKey)
+}
+
+function size(target) {
+  target = toRaw(target)
+  track(target, 'iterate', ITERATE_KEY)
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/size
+  // size 是在 Map 原型上的一个属性
+  return Reflect.get(getProto(target), 'size', target)
+}
+
+function add(value) {
+  value = toRaw(value)
+  const target = toRaw(this)
+  const proto = getProto(target)
+  const hadKey = proto.has.call(target, value)
+  const result = proto.add.call(target, value)
+  if (!hadKey) {
+    trigger(target, 'add', value, value)
+  }
+  return result
+}
+
+function createForEach(isReadonly, shallow) {
+  return function forEach(callback, thisArg) {
+    const observed = this
+    const target = toRaw(observed)
+
+    const wrap = isReadonly ? toReadonly : shallow ? toShallow : toReactive
+
+    !isReadonly && track(target, 'iterate', ITERATE_KEY)
+
+    // 封装的目的：
+    // 1. 确保在 thisArg 作用域下调用
+    // 2. 确保传递给 callback 的值都是 creative 的
+    function wrappedCallback(value, key) {
+      return callback.call(thisArg, wrap(value), wrap(key), observed)
+    }
+    return getProto(target).forEach.call(target, wrappedCallback)
+  }
+}
+
+function deleteEntry(key) {
+  const target = toRaw(this)
+  const { has, get, delete: del } = getProto(target)
+  const hadKey = has.call(target, key)
+  if (!hadKey) {
+    key = toRaw(key)
+    hadKey = has.call(target, key)
+  } else if (__DEV__) {
+    // TODO
+  }
+
+  const oldValue = get ? get.call(target, key) : undefined
+  const result = del.call(target, key)
+
+  if (hadKey) {
+    trigger(target, 'delete', key, undefined, oldValue)
+  }
+  return result
 }
 
 function set(key, value) {
@@ -45,15 +117,116 @@ function set(key, value) {
   return result
 }
 
+function clear() {
+  const target = toRaw(this)
+  const hadItems = target.size !== 0
+  const oldTarget = __DEV__
+    ? target instanceof Map
+      ? new Map(target)
+      : new Set(target)
+    : undefined
+
+  const result = getProto(target).clear.call(target)
+  if (hadItems) {
+    trigger(target, 'clear', undefined, undefined, oldTarget)
+  }
+  return result
+}
+
+// 只读函数，会改变对象的操作均不响应
+function createReadonlyMethod(type) {
+  return function (...args) {
+    if (__DEV__) {
+      const key = args[0] ? `on key "${args[0]}" ` : ``
+      console.warn(
+        `${type} operation ${key}failed: target is readonly.`,
+        toRaw(this)
+      )
+    }
+    return type === 'delete' ? false : this
+  }
+}
+
+function createIterableMethod(method, isReadonly, shallow) {
+  return function (...args) {
+    const target = toRaw(this)
+    const isMap = target instanceof Map
+    const isPair = method === 'entries' || (method === Symbol.iterator && isMap)
+    const isKeyOnley = method === 'keys' && isMap
+    const innerIterator = getProto(target)[method].apply(target, args)
+    const wrap = isReadonly ? toReadonly : shallow ? toShallow : toReactive
+    !isReadonly &&
+      track(target, 'iterate', isKeyOnley ? MAP_KEY_ITERATE_KEY : ITERATE_KEY)
+
+    return {
+      next() {
+        // 原本的迭代器
+        const { value, done } = innerIterator.next()
+        return done
+          ? { value, done }
+          : {
+              value: isPair ? [wrap(value[0], wrap(value[1]))] : wrap(value),
+              done
+            }
+      },
+      // 可迭代对象实现基础
+      [Symbol.iterator]() {
+        return this
+      }
+    }
+  }
+}
+
 // proxy handlers 对象
 const mutableInstrumentations = {
   get(key) {
     return get(this, key, toReactive)
   },
-  set
+  set,
+  get size() {
+    return size(this)
+  },
+  has,
+  add,
+  clear,
+  delete: deleteEntry,
+  forEach: createForEach(false, false)
 }
-const shallowInstrumentations = {}
-const readonlyInstrumentations = {}
+const shallowInstrumentations = {
+  get(key) {
+    return get(this, key, toShallow)
+  },
+  get size() {
+    return size(this)
+  },
+  has,
+  add,
+  set,
+  delete: deleteEntry,
+  clear,
+  forEach: createForEach(false, true)
+}
+const readonlyInstrumentations = {
+  get(key) {
+    return get(this, key, toReadonly)
+  },
+  get size() {
+    return size(this)
+  },
+  has,
+  add: createReadonlyMethod('add'),
+  set: createReadonlyMethod('set'),
+  delete: createReadonlyMethod('delete'),
+  clear: createReadonlyMethod('clear'),
+  forEach: createForEach(true, false)
+}
+
+const iteratorMethods = ['keys', 'values', 'entries', Symbol.iterator]
+iteratorMethods.forEach((method) => {
+  mutableInstrumentations[method] = createIterableMethod(method, false, false)
+  readonlyInstrumentations[method] = createIterableMethod(method, true, false)
+  shallowInstrumentations[method] = createIterableMethod(method, false, true)
+})
 
 function createInstrumentationGetter(isReadonly, shallow) {
   // 决定使用哪种类型的 instru...
