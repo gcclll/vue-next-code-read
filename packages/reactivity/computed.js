1709bbca5a1f7362d3dca3485fa8aa9314459f76
gcclll <gccll.love@gmail.com>
2020-05-27 14:44:20 +0800

    over reactive

--- packages/reactivity/computed.js
+++ packages/reactivity/computed.js
@@ -1,13 +1,14 @@
-import { __DEV__, effect, ReactiveEffect, trigger, track } from './effect.js'
+import { __DEV__, effect, trigger, track, targetMap } from './effect.js'
 const noop = () => {}
 
-export function computed(getterOrOptions) {
+export function computed(getterOrOptions, id) {
   let getter, setter
 
   if (typeof getterOrOptions === 'function') {
     getter = getterOrOptions
     setter = __DEV__
       ? () => {
+          // 为了测试用例方便改成抛错误
           throw new Error('计算属性只读。')
         }
       : noop
