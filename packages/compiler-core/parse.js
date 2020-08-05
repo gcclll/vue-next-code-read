2a4a02a720d872c4f52c81d9c8e9e73884060d20
gcclll <gccll.love@gmail.com>
2020-08-05 22:41:17 +0800

    update

--- packages/compiler-core/parse.js
+++ packages/compiler-core/parse.js
@@ -1,22 +1,22 @@
 // import { NO } from "../util.js";
-import { createRoot, NodeTypes, Namespaces } from "./ast.js";
-import { advancePositionWithMutation } from "./utils.js";
-import { ErrorCodes, createCompilerError, defaultOnError } from "./error.js";
+import { createRoot, NodeTypes, Namespaces } from './ast.js'
+import { advancePositionWithMutation } from './utils.js'
+import { ErrorCodes, createCompilerError, defaultOnError } from './error.js'
 
-const NO = 0;
+const NO = 0
 const TagType = {
   Start: 0,
-  End: 1,
-};
+  End: 1
+}
 
-const decodeRE = /&(gt|lt|amp|apos|quot);/g;
+const decodeRE = /&(gt|lt|amp|apos|quot);/g
 const decodeMap = {
-  gt: ">",
-  lt: "<",
-  amp: "&",
+  gt: '>',
+  lt: '<',
+  amp: '&',
   apos: "'",
-  quot: '"',
-};
+  quot: '"'
+}
 
 // 文本节点模式
 export const TextModes = {
@@ -25,11 +25,11 @@ export const TextModes = {
   RCDATA: 1, //  | ✘        | ✔        | End tag of the parent | <textarea>
   RAWTEXT: 2, // | ✘        | ✘        | End tag of the parent | <style>,<script>
   CDATA: 3,
-  ATTRIBUTE_VALUE: 4,
-};
+  ATTRIBUTE_VALUE: 4
+}
 
 export const defaultParserOptions = {
-  delimiters: ["{{", "}}"], // 插值默认符号
+  delimiters: ['{{', '}}'], // 插值默认符号
   getNamespace: () => Namespaces.HTML,
   getTextMode: () => TextModes.DATA,
   isVoidTag: NO,
@@ -37,26 +37,26 @@ export const defaultParserOptions = {
   isCustomElement: NO,
   decodeEntities: (rawText) =>
     rawText.replace(decodeRE, (_, p1) => decodeMap[p1]),
-  onError: defaultOnError,
-};
+  onError: defaultOnError
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                  b1. 主要函数                              //
 ///////////////////////////////////////////////////////////////////////////////
 export function baseParse(content, options /* ParserOptions */) {
-  const context = createParserContext(content, options);
-  const start = getCursor(context);
+  const context = createParserContext(content, options)
+  const start = getCursor(context)
   return createRoot(
     parseChildren(context, TextModes.DATA, []),
     getSelection(context, start)
-  );
+  )
 }
 
 function createParserContext(context, options) /*ParserContext*/ {
   return {
     options: {
       ...defaultParserOptions,
-      ...options,
+      ...options
     },
     column: 1,
     line: 1,
@@ -64,8 +64,8 @@ function createParserContext(context, options) /*ParserContext*/ {
     originalSource: context,
     source: context,
     inPref: false,
-    inVPref: false,
-  };
+    inVPref: false
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -77,16 +77,16 @@ function parseChildren(
   ancestors /*ElementNode[]*/
 ) {
   // ...
-  const parent = last(ancestors);
-  const ns = parent ? parent.ns : Namespaces.HTML;
-  const nodes /*TemplateChildNode[]*/ = [];
+  const parent = last(ancestors)
+  const ns = parent ? parent.ns : Namespaces.HTML
+  const nodes /*TemplateChildNode[]*/ = []
 
-  let i = 0;
+  let i = 0
   while (!isEnd(context, mode, ancestors)) {
     // do sth
 
-    const s = context.source;
-    let node = undefined;
+    const s = context.source
+    let node = undefined
 
     // 由于 baseparse里面传过来的是个 DATA 类型，因此会走到这个 if 里
     // 面去解析
@@ -94,50 +94,50 @@ function parseChildren(
       // 过略掉非文本的
       if (!context.inVPre && s.startsWith(context.options.delimiters[0])) {
         // ... 插值处理{{}}
-        node = parseInterpolation(context, mode);
-      } else if (mode === TextModes.DATA && s[0] === "<") {
+        node = parseInterpolation(context, mode)
+      } else if (mode === TextModes.DATA && s[0] === '<') {
         // ... 标签开头 <...
         if (s.length === 1) {
-          emitError(context, ErrorCodes.EOF_BEFORE_TAG_NAME, 1);
-        } else if (s[1] === "!") {
+          emitError(context, ErrorCodes.EOF_BEFORE_TAG_NAME, 1)
+        } else if (s[1] === '!') {
           // TODO 注释处理，<!-- ...
-        } else if (s[1] === "/") {
+        } else if (s[1] === '/') {
           // </...
           if (s.length === 2) {
-            emitError(context, ErrorCodes.EOF_BEFORE_TAG_NAME, 2);
-          } else if (s[2] === ">") {
+            emitError(context, ErrorCodes.EOF_BEFORE_TAG_NAME, 2)
+          } else if (s[2] === '>') {
             // </> 不带标签名的无效标签
-            emitError(context, ErrorCodes.MISSING_END_TAG_NAME, 2);
+            emitError(context, ErrorCodes.MISSING_END_TAG_NAME, 2)
             // 过滤掉 </> 这三个字符串，offset>>3 退出本次循环继续解析
-            advanceBy(context, 3);
-            continue;
+            advanceBy(context, 3)
+            continue
           } else if (/[a-z]/i.test(s[2])) {
             // 这里都出错了，为啥后面还有个 parseTag ???
-            emitError(context, ErrorCodes.X_INVALID_END_TAG);
-            parseTag(context, TagType.End, parent);
-            continue;
+            emitError(context, ErrorCodes.X_INVALID_END_TAG)
+            parseTag(context, TagType.End, parent)
+            continue
           } else {
             emitError(
               context,
               ErrorCodes.INVALID_FIRST_CHARACTER_OF_TAG_NAME,
               2
-            );
+            )
             // node = parseBogusComment(context)
           }
         } else if (/[a-z]/i.test(s[1])) {
           // 解析起始标签，即这里才是标签最开始的位置。
           // node = parseElement(context, ancestors);
-        } else if ([s[1] === "?"]) {
+        } else if ([s[1] === '?']) {
           // <? 开始的
           emitError(
             context,
             ErrorCodes.UNEXPECTED_QUESTION_MARK_INSTEAD_OF_TAG_NAME,
             1
-          );
+          )
           // node = parseBogusComment(context)
         } else {
           // 其他情况都视为非法
-          emitError(context, ErrorCodes.INVALID_FIRST_CHARACTER_OF_TAG_NAME, 1);
+          emitError(context, ErrorCodes.INVALID_FIRST_CHARACTER_OF_TAG_NAME, 1)
         }
       }
 
@@ -146,94 +146,94 @@ function parseChildren(
     }
 
     if (!node) {
-      node = parseText(context, mode);
+      node = parseText(context, mode)
     }
 
     if (Array.isArray(node)) {
       for (let i = 0; i < node.length; i++) {
-        pushNode(nodes, node[i]);
+        pushNode(nodes, node[i])
       }
     } else {
-      pushNode(nodes, node);
+      pushNode(nodes, node)
     }
   }
 
-  let removedWhitespace = false;
+  let removedWhitespace = false
 
-  return removedWhitespace ? nodes.filter(Boolean) : nodes;
+  return removedWhitespace ? nodes.filter(Boolean) : nodes
 }
 
 function parseText(context, mode) {
-  const endTokens = ["<", context.options.delimiters[0]];
+  const endTokens = ['<', context.options.delimiters[0]]
   if (mode === TextModes.CDATA) {
-    endTokens.push("]]>");
+    endTokens.push(']]>')
   }
 
-  let endIndex = context.source.length;
+  let endIndex = context.source.length
   for (let i = 0; i < endTokens.length; i++) {
-    const index = context.source.indexOf(endTokens[i], 1);
+    const index = context.source.indexOf(endTokens[i], 1)
     if (index !== -1 && endIndex > index) {
-      endIndex = index;
+      endIndex = index
     }
   }
 
-  const start = getCursor(context);
-  const content = parseTextData(context, endIndex, mode);
+  const start = getCursor(context)
+  const content = parseTextData(context, endIndex, mode)
   return {
     type: NodeTypes.TEXT,
     content,
-    loc: getSelection(context, start),
-  };
+    loc: getSelection(context, start)
+  }
 }
 
 // 解析文本数据，纯文本内容
 function parseTextData(context, length, mode) {
-  const rawText = context.source.slice(0, length);
+  const rawText = context.source.slice(0, length)
   // 解析换行，更新 line, column, offset，返回换行之后的的 source
-  advanceBy(context, length);
+  advanceBy(context, length)
   if (
     mode === TextModes.RAWTEXT ||
     mode === TextModes.CDATA ||
-    rawText.indexOf("&") === -1
+    rawText.indexOf('&') === -1
   ) {
-    return rawText;
+    return rawText
   }
 
   return context.options.decodeEntities(
     rawText,
     mode === TextModes.ATTRIBUTE_VALUE
-  );
+  )
 }
 
 function parseTag(context, type, parent) {
   // 获取当前解析的起始位置，此时值应该是 some text 的长度
-  const start = getCursor(context);
+  const start = getCursor(context)
   // 匹配 </div 过滤掉空格字符，但是为什么要把 > 给忽略掉???
-  const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source);
-  const tag = match[1];
-  const ns = context.options.getNamespace(tag, parent);
+  const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source)
+  const tag = match[1]
+  const ns = context.options.getNamespace(tag, parent)
   // log1: 改变位移，将 offset 定位到 </div> 的最有一个 > 上
   // 在这里 context.offset = 10, context.line = 1
-  advanceBy(context, match[0].length);
+  advanceBy(context, match[0].length)
   // 过滤掉空格
-  advanceSpaces(context);
+  advanceSpaces(context)
   // log2: 经过 advance之后 context.offset = 15, context.line = 1
   // 正好过滤 </div 5个字符
-  const cursor = getCursor(context);
-  const currSource = context.source;
+  const cursor = getCursor(context)
+  const currSource = context.source
 }
 
 function parseInterpolation(context, mode) {
   // 找出插值模板的开始和结束符号，默认是 {{ 和 }}
-  const [open, close] = context.options.delimiters;
-  const closeIndex = context.source.indexOf(close, open.length);
+  const [open, close] = context.options.delimiters
+  const closeIndex = context.source.indexOf(close, open.length)
   if (closeIndex === -1) {
-    emitError(context, ErrorCodes.X_MISSING_INTERPOLATION_END);
-    return undefined;
+    emitError(context, ErrorCodes.X_MISSING_INTERPOLATION_END)
+    return undefined
   }
 
-  const start = getCursor(context);
-  advanceBy(context, open.length);
+  const start = getCursor(context)
+  advanceBy(context, open.length)
 
   // 下面是从 {{ 之后的字符串开始解析
   const innerStart = getCursor(context),
@@ -244,9 +244,9 @@ function parseInterpolation(context, mode) {
     rawContent = context.source.slice(0, rawContentLength),
     preTrimContent = parseTextData(context, rawContentLength, mode),
     content = preTrimContent.trim(),
-    startOffset = preTrimContent.indexOf(content);
+    startOffset = preTrimContent.indexOf(content)
   if (startOffset > 0) {
-    advancePositionWithMutation(innerStart, rawContent, startOffset);
+    advancePositionWithMutation(innerStart, rawContent, startOffset)
   }
 
   // {{ foo + bar }} ->
@@ -254,12 +254,12 @@ function parseInterpolation(context, mode) {
   // 插值里面字符串的长度 - 去掉空格后的长度 - 起始空格的长度，得到的
   // 就是结束位置的 offset
   const endOffset =
-    rawContentLength - (preTrimContent.length - content.length - startOffset);
-  advancePositionWithMutation(innerEnd, rawContent, endOffset);
+    rawContentLength - (preTrimContent.length - content.length - startOffset)
+  advancePositionWithMutation(innerEnd, rawContent, endOffset)
   // 定位到 }} 位置
-  advanceBy(context, close.length);
+  advanceBy(context, close.length)
 
-  console.log(innerEnd, innerStart, "1");
+  console.log(innerEnd, innerStart, '1')
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
@@ -267,10 +267,10 @@ function parseInterpolation(context, mode) {
       isStatic: false,
       isConstant: false,
       content,
-      loc: getSelection(context, innerStart, innerEnd),
+      loc: getSelection(context, innerStart, innerEnd)
     },
-    loc: getSelection(context, start),
-  };
+    loc: getSelection(context, start)
+  }
 }
 ///////////////////////////////////////////////////////////////////////////////
 //                               b3.辅助类函数                                //
@@ -281,24 +281,24 @@ function isEnd(
   mode /*TextModes*/,
   ancestors /*ElementNode[]*/
 ) /*boolean*/ {
-  const s = context.source;
+  const s = context.source
 
   // mode 为 TextModes 各种情况
   // ...省略
   switch (mode) {
     case TextModes.DATA:
-      if (s.startsWith("</")) {
+      if (s.startsWith('</')) {
         // 标签
         for (let i = ancestors.length - 1; i >= 0; --i) {
           if (startsWithEndTagOpen(s, ancestors[i].tag)) {
-            return true;
+            return true
           }
         }
       }
   }
 
   // 是 TextModes.TEXT 直接返回 source 的内容是否为空了
-  return !s;
+  return !s
 }
 
 // 获取当前被解析模板的位置信息
@@ -306,8 +306,8 @@ function isEnd(
 // line 当前解析位置的行
 // 当前解析的偏移量 offset，已解析的字符串长度
 function getCursor(context /*ParserContext*/) /*Position*/ {
-  const { column, line, offset } = context;
-  return { column, line, offset };
+  const { column, line, offset } = context
+  return { column, line, offset }
 }
 
 function getSelection(
@@ -315,75 +315,75 @@ function getSelection(
   start /*Position*/,
   end /*Position*/
 ) /*SourceLocation*/ {
-  end = end || getCursor(context);
+  end = end || getCursor(context)
   return {
     start,
     end,
-    source: context.originalSource.slice(start.offset, end.offset),
-  };
+    source: context.originalSource.slice(start.offset, end.offset)
+  }
 }
 
 // 匹配：</tag> 或</tag 没有 `>` 的情况???
 function startsWithEndTagOpen(source, tag) {
   return (
-    source.startsWith("</") &&
+    source.startsWith('</') &&
     source.substr(2, tag.length).toLowerCase() === tag.toLowerCase() &&
-    /[\t\n\f />]/.test(source[2 + tag.length] || ">")
-  );
+    /[\t\n\f />]/.test(source[2 + tag.length] || '>')
+  )
 }
 
 function pushNode(nodes, node) {
   if (node.type === NodeTypes.COMMENT) {
     // 注释节点不处理
-    return;
+    return
   }
 
   if (node.type === NodeTypes.TEXT) {
     // 合并文本，前提是两个靠着的文本节点 -> end.offset === start.offset
-    const prev = last(nodes);
+    const prev = last(nodes)
     if (
       prev &&
       prev.type === NodeTypes.TEXT &&
       prev.loc.end.offset === node.loc.start.offset
     ) {
-      prev.content += node.content;
-      prev.loc.end = node.loc.end;
-      prev.loc.source += node.loc.source;
-      return;
+      prev.content += node.content
+      prev.loc.end = node.loc.end
+      prev.loc.source += node.loc.source
+      return
     }
   }
 
-  return nodes.push(node);
+  return nodes.push(node)
 }
 
 function last(ns) {
-  return ns[ns.length - 1];
+  return ns[ns.length - 1]
 }
 
 // 过滤掉空格
 function advanceSpaces(context) {
-  const match = /^[\t\r\n\f ]+/.exec(context.source);
+  const match = /^[\t\r\n\f ]+/.exec(context.source)
   if (match) {
-    advanceBy(context, match[0].length);
+    advanceBy(context, match[0].length)
   }
 }
 
 function advanceBy(context, numberOfCharacters) {
-  const { source } = context;
-  advancePositionWithMutation(context, source, numberOfCharacters);
-  context.source = source.slice(numberOfCharacters);
+  const { source } = context
+  advancePositionWithMutation(context, source, numberOfCharacters)
+  context.source = source.slice(numberOfCharacters)
 }
 
 function emitError(context, code, offset, loc = getCursor(context)) {
   if (offset) {
-    loc.offset += offset;
-    loc.column += offset;
+    loc.offset += offset
+    loc.column += offset
   }
   context.options.onError(
     createCompilerError(code, {
       start: loc,
       end: loc,
-      source: "",
+      source: ''
     })
-  );
+  )
 }
